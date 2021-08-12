140181dcbae0db592036d020e0849b9d116363d5
jblici <juambif@gmail.com>
2021-08-12 15:23:04 -0300

    latest products and product detail

--- Ecommerce_django/product/models.py
+++ Ecommerce_django/product/models.py
@@ -1,7 +1,9 @@
-from django.db import models
+ 
 from io import BytesIO
 from PIL import Image
+
 from django.core.files import File
+from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
@@ -9,10 +11,10 @@ class Category(models.Model):
 
     class Meta:
         ordering = ('name',)
-
+    
     def __str__(self):
         return self.name
-
+    
     def get_absolute_url(self):
         return f'/{self.slug}/'
 
@@ -28,18 +30,18 @@ class Product(models.Model):
 
     class Meta:
         ordering = ('-date_added',)
-
+    
     def __str__(self):
         return self.name
-
+    
     def get_absolute_url(self):
         return f'/{self.category.slug}/{self.slug}/'
-
+    
     def get_image(self):
         if self.image:
             return 'http://127.0.0.1:8000' + self.image.url
         return ''
-
+    
     def get_thumbnail(self):
         if self.thumbnail:
             return 'http://127.0.0.1:8000' + self.thumbnail.url
@@ -51,15 +53,15 @@ class Product(models.Model):
                 return 'http://127.0.0.1:8000' + self.thumbnail.url
             else:
                 return ''
-
-    def make_thumbnail(self, image, size=(300,200)):
+    
+    def make_thumbnail(self, image, size=(300, 200)):
         img = Image.open(image)
         img.convert('RGB')
         img.thumbnail(size)
 
         thumb_io = BytesIO()
         img.save(thumb_io, 'JPEG', quality=85)
-        
+
         thumbnail = File(thumb_io, name=image.name)
 
         return thumbnail
